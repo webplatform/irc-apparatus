e5f890cda9f295263bc734ab204a69b39ab64129
David Kirstein <frozenice@frozenice.de>
2014-01-07 23:31:44 +0100

    [test][Bot] Add more config tests
    Also, change Travis test output back to spec when using Coveralls, since we have a workaround for the mocha-multi process killing now.

--- test/bot/config.js
+++ test/bot/config.js
@@ -1,4 +1,39 @@
+var _ = require('lodash-node');
 var app = require('../../index.js');
+
+var defaultConfig = {
+  bot: {
+    plugins: []
+  },
+
+  irc: {
+    server: null,
+    port: 6667,
+    secure: false,
+    password: null,
+    nick: null,
+    userName: null,
+    realName: null,
+    channels: [],
+    retryCount: 10
+  }
+};
+
+var customConfig = {
+  bot: {
+    plugins: ['plugin1', 'plugin2']
+  },
+  irc: {
+    server: 'irc.freenode.net',
+    port: 6697,
+    secure: true,
+    nick: 'testnick',
+    userName: 'testusername',
+    realName: 'testrealname',
+    channels: ['#channel1', '#channel2'],
+    retryCount: 5
+  }
+};
 
 describe('The Bot config', function() {
   describe('with default settings', function() {
@@ -28,5 +63,21 @@ describe('The Bot config', function() {
         retryCount: 10
       });
     });
+  });
+
+  describe('with custom settings via object', function() {
+    var bot;
+
+    before(function() {
+      bot = app.Bot(customConfig);
+    });
+
+    it('should be initialized with the custom config', function() {
+      var expected = _.merge(_.cloneDeep(defaultConfig), customConfig);
+      bot.conf('bot').should.deep.equal(expected.bot);
+      bot.conf('irc').should.deep.equal(expected.irc);
+    });
+
+    // TODO: change values, reload
   });
 });
